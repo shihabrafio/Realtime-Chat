ba5b41934a9584948a8a86f4af1b7f52d5a90c95
shihabrafio <aimshihabuddinkhan@gmail.com>
2024-04-13 20:29:22 +0600

    Add PrettyChatWindow component to ChatsPage

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { PrettyChatWindow } from 'react-chat-engine-pretty';
 
-export const ChatsPage = () => {
+export const ChatsPage = (props) => {
   return (
-    <div>ChatsPage</div>
+    <div className='background'>
+      <PrettyChatWindow 
+        projectId={import.meta.env.project_id}
+        username={props.user.username}
+        secret={props.user.secret}
+        style={{ height: '100vh' }}
+      />
+    </div>
   )
 }
