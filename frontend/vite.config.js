307956bc71c3b364c538cbabe2c30eeae2fc4ac0
shihabrafio <aimshihabuddinkhan@gmail.com>
2024-04-14 20:58:24 +0600

    Update package.json and vite.config.js to use commonjs module type

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-import vitePluginRequire from "vite-plugin-require";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),vitePluginRequire.default()],
+  base:"/realtime-chat/"
+  plugins: [react(),],
 
 })
