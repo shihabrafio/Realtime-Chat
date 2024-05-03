e2ed0f12a7b1eda4d1485be8224f5e29a802eab2
Shihab Uddin Khan <aimshihabuddinkhan@gmail.com>
2024-05-03 17:53:52 +0000

    Add password

--- frontend/src/AuthPage.jsx
+++ frontend/src/AuthPage.jsx
@@ -1,27 +1,48 @@
 import axios from 'axios'
+import React, { useState } from 'react';
 
 export const AuthPage = (props) => {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
     const onSubmit =(e)=>{
         e.preventDefault()
         console.log(e)
         const {value} = e.target[0]
-        axios.post('https://realtime-chat-hrxj.onrender.com/authenticate',{username: value})
+        axios.post('https://realtime-chat-hrxj.onrender.com/authenticate',{username,password})
         .then(r=>props.onAuth({...r.data, secret: value}))
         .catch(e=>console.log(e))
     }
   return (
-    <div className='background'>
-        <form onSubmit={onSubmit} className='form-card'>
-            <div className='form-title'> Welcome </div>
-            <div className='form-subtitle'> Set a username to get started </div>
+        <div className='background'>
+            <form onSubmit={onSubmit} className='form-card'>
+                <div className='form-title'> Welcome </div>
+                <div className='form-subtitle'> Set a username and password to get started </div>
 
-            <div className='auth'>
-                <div className='auth-label'>Username</div>
-                <input className='auth-input' name='username'/>
+                <div className='auth'>
+                    <div className='auth-label'>Username</div>
+                    <input
+                        className='auth-input'
+                        name='username'
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                </div>
+
+                <div className='auth'>
+                    <div className='auth-label'>Password</div>
+                    <input
+                        className='auth-input'
+                        name='password'
+                        type='password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+
                 <button className='auth-button' type='submit'>Enter</button>
-            </div>
-        </form>
-
-    </div>
+            </form>
+        </div>
   )
 }
