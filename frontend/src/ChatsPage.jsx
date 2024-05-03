bd3babd20952f252856034fb4e9c3bbc5627fbcc
Shihab Uddin Khan <aimshihabuddinkhan@gmail.com>
2024-05-03 17:09:04 +0000

    Add project ID

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -4,7 +4,7 @@ export const ChatsPage = (props) => {
   return (
     <div className='background'>
       <PrettyChatWindow 
-        projectId={import.meta.env.VITE_CHAT_ENGINE_PROJECT_ID}
+        projectId="b735fdc3-f333-4f9a-ad8e-75a564bf3e59"
         username={props.user.username}
         secret={props.user.secret}
         style={{ height: '100vh' }}
