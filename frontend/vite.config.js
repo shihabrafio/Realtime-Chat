fa2a515e0d5fbe1b5881a1919210e3a2c05a4d79
Shihab Uddin Khan <aimshihabuddinkhan@gmail.com>
2024-05-03 17:17:08 +0000

    set up the base

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/realtime-chat/",
+  base:"https://realtime-chat-frontend-2ync.onrender.com/",
   plugins: [react(),],
   build: {
     target: 'modules',
